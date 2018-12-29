73cf1ca517476e11cded4464f2b3ec141bfe0cc8
farawayCC <iosdevfarconsoft@gmail.com>
2018-12-29 23:09:30 +0100

    Factory and index.js files added

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import CampaignFactory from './build/ProductFactory.json';
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0x65A61D84EC86550311d4002E18F009D3A5B7600a'
+);
+
+export default instance;
