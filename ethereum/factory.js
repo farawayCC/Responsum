73b058c83ca7b269068467fa8580e4402d8d544f
farawayCC <iosdevfarconsoft@gmail.com>
2019-01-02 21:07:12 +0100

    new factory address+babel

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import ProductFactory from './build/ProductFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(ProductFactory.interface),
-  '0x242a451d7305ef79173bf9758826f64632b21c6f'
+  '0x28ef0f85e15f7c1084e5d28df04834109095f4e0'
 );
 
 export default instance;
