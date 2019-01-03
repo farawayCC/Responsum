a91e050a60b7ad9473a9391a3abd9918051bd420
farawayCC <iosdevfarconsoft@gmail.com>
2019-01-03 20:40:24 +0100

    More Ui improvements

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -6,24 +6,23 @@ export default props => {
   // console.log("-----------------------------------")
   // console.log(props.review);
   const finalElement = (
-    <Grid celled>
-      <Link route={`/products/${props.address}/reviews/${props.index}`}>
-        <a>
-          <Grid.Column width={2}>
-              <Image src={props.review.photoLink} />
-          </Grid.Column>
 
-          <Grid.Column width={8}>
-            <Container>
-              <Header as='h1'> <a>{props.review.header}</a> </Header>
-              <Rating icon='star' defaultRating={props.review.rate} maxRating={5} disabled />
-              <Header as='h3'>{props.review.text}</Header>
-              <Header as='h3' >Author address: {props.review.author}</Header>
-            </Container>
+    <Link route={`/products/${props.address}/reviews/${props.index}`}>
+      <a>
+        <Grid celled>
+          <Grid.Column width={5}>
+            <Image src={props.review.photoLink} />
           </Grid.Column>
+          <Grid.Column width={11}>
+            <Header as='h1'> <a>{props.review.header}</a> </Header>
+            <Rating icon='star' defaultRating={props.review.rate} maxRating={5} disabled />
+            <Header as='h3'>{props.review.text}</Header>
+            <Header as='h3' >Author address: {props.review.author}</Header>
+          </Grid.Column>
+        </Grid>
       </a>
-      </Link>
-    </Grid>
+    </Link>
+
   );
   return finalElement;
 };
