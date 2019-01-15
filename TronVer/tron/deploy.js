aa89d51ff19ffdd00cd28bf635ba1f35cb28b504
farawayCC <iosdevfarconsoft@gmail.com>
2019-01-15 09:59:13 +0100

    compile script code moved to deploy

--- TronVer/tron/deploy.js
+++ TronVer/tron/deploy.js
@@ -1,22 +1,83 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const compiledFactory = require('./build/ProductFactory.json');
+const solc = require('solc');
+const fs = require('fs-extra');
+const TronWeb = require('TronWeb');
+const config = require('./config.json');
 
-const provider = new HDWalletProvider(
-  'purchase idle erosion foam relief rose century turtle bulb correct country melt',
-  'https://rinkeby.infura.io/v3/40665b54b6c04deeb87c66e045626409'
-);
-const web3 = new Web3(provider);
+const CONTRACT_FOLDER = '../contracts/';
 
-const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from account', accounts[0]);
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
+const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
+const eventServer = 'https://api.shasta.trongrid.io/';
 
-  const result = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface))
-      .deploy({ data: '0x' + compiledFactory.bytecode })
-      .send({from: accounts[0] });
+const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, config.deploy_keys.testnet);
 
-    console.log('Contract deployed to', result.options.address);
-};
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function deploy() {
+  const input = {};
+
+  const files = fs.readdirSync(CONTRACT_FOLDER);
+  files.map((file) => {
+    input[file] = fs.readFileSync(CONTRACT_FOLDER + file, 'utf8').toString();
+  });
+
+  const output = {};
+
+  console.log(`Compiling solidity...`);
+  const compiled = solc.compile({sources: input}, 1);
+
+  if (!compiled.errors) {
+    for (let contractName in compiled.contracts) {
+      // code and ABI that are needed by web3
+      console.log(`deploying ${contractName}`);
+
+      const unsigned = await tronWeb.transactionBuilder.createSmartContract({
+        abi: compiled.contracts[contractName].interface,
+        bytecode: compiled.contracts[contractName].bytecode,
+      });
+      const signed = await tronWeb.trx.sign(unsigned);
+      const broadcastResult = await tronWeb.trx.sendRawTransaction(signed);
+
+      if (broadcastResult.result === true) {
+        let transactionInfo = {};
+        do {
+          transactionInfo = await tronWeb.trx.getTransactionInfo(signed.txID);
+          if (transactionInfo.id && transactionInfo.receipt.result === 'SUCCESS') {
+            if (transactionInfo.receipt.result) {
+              console.log(`SUCCESSFULLY deployed ${contractName}. Cost: ${transactionInfo.receipt.energy_fee / 1000000} TRX.`);
+              console.log(`Contract address: ${signed.contract_address}`);
+              output[contractName] = signed.contract_address;
+              output[contractName] = {
+                address: signed.contract_address,
+                abi: JSON.parse(compiled.contracts[contractName].interface)
+              }
+            } else {
+              console.log(`FAILED deploying ${contractName}. Cost: ${transactionInfo.receipt.energy_fee / 1000000} TRX.`);
+              console.log(`transaction info:`);
+              console.log(transactionInfo);
+              process.exit();
+            }
+          }
+          await sleep(2000);
+        } while (!transactionInfo.id);
+      } else {
+        console.log(`FAILED to broadcast ${contractName} deploy transaction`);
+        console.log((broadcastResult));
+        process.exit();
+      }
+    }
+
+    fs.writeFileSync('../src/config/contracts.json', JSON.stringify(output, null, 4));
+  } else {
+    console.log(`FAILED to compile solidity...`);
+    compiled.errors.map((e) => {
+      console.log(e);
+    });
+  }
+}
+
 deploy();
